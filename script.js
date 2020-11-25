0106f6346c5a6291853da309dbf37fe9b1b2fc5d
Besson Mikael <73292500+MikaelBesson@users.noreply.github.com>
2020-11-25 11:18:27 +0100

    exo 54

---
+++ script.js
@@ -0,0 +1,13 @@
+let x = 1;
+let y = 4;
+let z = -1;
+
+if( x < 2);
+if( y > 3);
+if( z < 0);
+
+let a =document.getElementById("a")
+
+if( x < 2 && y > 4 && z < 0) {
+    a.innerHTML = "x est inferieur 1 y est plus grand que 3 et z inferieur a 0";
+}
